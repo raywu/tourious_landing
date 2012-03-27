9be28777d9c796855aa4e2898254e28266155708
Ray Wu <raymond.cj.wu@gmail.com>
2012-03-26 22:26:44 -0400

    created user with firstname lastname email

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TouriousLanding::Application.config.secret_token = 'c28b02bcd505857a431b877fa86f31d463b830b214f09c92ea4492a161c9279471a5b241bea359a42c507f001523c766f557977514099be9deadb7ff57ca3592'
